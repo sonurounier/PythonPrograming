9d1b1967d41fbd4ea63ff383400c94a5db032d23
sonurounier <sonu.rounier@gmail.com>
2018-09-21 01:47:44 +0530

    Merge branch 'master' of https://github.com/sonurounier/PythonPrograming

--- S01AQ03-Mutliplication_table_user_input.py
+++ S01AQ03-Mutliplication_table_user_input.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """ 
 This program prints multiplication based on user input
 """
@@ -11,3 +12,18 @@ def print_mtable():
 
 # Main starts from here
 print_mtable()
+=======
+""" 
+This program prints multiplication based on user input
+"""
+
+def print_mtable():
+    #This function print multiplication table based on user input
+    Multiply_num=int(input("Please Enter the number for Multiplication table:"))
+    for i in range(1,11):
+        #print(Multiply_num,"x",i,"=",Multiply_num*i)
+        print(Multiply_num,"{: >1}".format("x"),"{: >2}".format(i),"{: >2}".format("="),"{: >3}".format(Multiply_num*i))
+
+# Main starts from here
+print_mtable()
+>>>>>>> 35981c62382cc5bc47088ac8f040e7790dd3550f
